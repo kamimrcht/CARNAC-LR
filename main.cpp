ecd72d24a4184b172c7d0943b5ba49885c0d2137
MARCHET Camille <camille.marchet@inria.fr>
2017-05-31 14:52:19 +0200

    end of cleaning and added main

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <iterator>
+#include <ctime>
+#include <unordered_map>
+#include <algorithm>
+#include <cmath>
+#include <chrono>
+#include <unordered_set>
+#include <set>
+#include <algorithm>
+#include <map>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sstream>
+#include <mutex>
+#include <functional>
+#include <utility>
+#include "clustering_cliqueness.hpp"
+
+int main(int argc, char** argv){
+	bool cmd(true);
+	if (argc > 1){
+		cmd = execute(argc, argv);
+	}
+	printHelpCmd(cmd);
+	return 0;
+}
